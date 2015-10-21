d10e3d511c52da541832ef2342904404bbb01e43
Martin Heidegger <martin.heidegger@gmail.com>
2015-10-22 01:34:02 +0900

    Replaced msee dependency with msee of workshopper-adventure

--- problem.js
+++ problem.js
@@ -1,19 +1,15 @@
-var msee = require('msee'),
-    path = require('path'),
+var path = require('path'),
     verify = require('adventure-verify');
 
  module.exports = function(dir, testCorrect) {
  
    var problem = {
     init: function (exercise) {
-       ['problem', 'solution', 'pass', 'fail'].forEach(function(type) {
-          problem[type] = function () {
-            return msee.parseFile(
-              dir + '/' + exercise.lang + '/' + type + '.md',
-               {paragraphEnd: '\n\n'}
-             );
-          }
-         });
+      ['problem', 'solution', 'pass', 'fail'].forEach(function(type) {
+        problem[type] = {
+          file: path.join(dir, exercise.i18n.lang(), type + '.md')
+        }
+      });
     },
     verify: verify({ modeReset: true }, testCorrect)
   };
