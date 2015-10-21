a169d90c1be4579816056bd8652e7187f14cb1f8
Martin Heidegger <martin.heidegger@gmail.com>
2015-10-22 01:34:02 +0900

    Removed msee dependency

--- problem.js
+++ problem.js
@@ -1,5 +1,4 @@
-var msee = require('msee'),
-    path = require('path'),
+var path = require('path'),
     verify = require('adventure-verify');
 
 module.exports = function(dir, testCorrect) {
@@ -7,11 +6,8 @@ module.exports = function(dir, testCorrect) {
   var problem = {
     init: function (exercise) {
       ['problem', 'solution', 'pass', 'fail'].forEach(function(type) {
-          problem[type] = function () {
-            return msee.parseFile(
-              dir + '/' + exercise.lang + '/' + type + '.md',
-              {paragraphEnd: '\n\n'}
-            );
+          problem[type] = {
+            file: path.join(dir, exercise.i18n.lang(), type + '.md')
           }
         });
     },
