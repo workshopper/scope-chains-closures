8bc2f213838d3239e7069311fe55cabb295caeb5
Martin Heidegger <martin.heidegger@gmail.com>
2015-10-22 02:22:20 +0900

    Added basic translation system

--- problem.js
+++ problem.js
@@ -2,18 +2,22 @@ var msee = require('msee'),
     path = require('path'),
     verify = require('adventure-verify');
 
-module.exports = function(dir, testCorrect) {
-
-  var problem = {
+ module.exports = function(dir, testCorrect) {
+ 
+   var problem = {
+    init: function (exercise) {
+       ['problem', 'solution', 'pass', 'fail'].forEach(function(type) {
+          problem[type] = function () {
+            return msee.parseFile(
+              dir + '/' + exercise.lang + '/' + type + '.md',
+               {paragraphEnd: '\n\n'}
+             );
+          }
+         });
+    },
     verify: verify({ modeReset: true }, testCorrect)
   };
+ 
+   return problem;
+ };
 
-  ['problem', 'solution', 'pass', 'fail'].forEach(function(type) {
-    problem[type] = msee.parseFile(
-      dir + '/' + type + '.md',
-      {paragraphEnd: '\n\n'}
-    );
-  });
-
-  return problem;
-};
